11ae58fa18d8337a36d48dba1d0e543fb90118a3
huda91nidhal <hudaalshomar@gmail.com>
2026-05-29 14:38:11 +0300

    Add files via upload

---
+++ Project 1 /Extension 1 - DbLinkedList Size/level5#project1.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include "clsDbLinkedList.h"
+using namespace std;
+int main()
+{
+	clsDbLinkedList <int> MyDbLinkedList;
+
+	MyDbLinkedList.InsertAtBeginning(5);
+	MyDbLinkedList.InsertAtBeginning(4);
+	MyDbLinkedList.InsertAtBeginning(3);
+	MyDbLinkedList.InsertAtBeginning(2);
+	MyDbLinkedList.InsertAtBeginning(1);
+
+	cout << "Linked List Content :\n";
+	MyDbLinkedList.PrintList();
+
+	cout << "\nNumber of items in the linked list = " << MyDbLinkedList.Size();
+
+
+	
+	
+	system("pause > 0");
+
+}
