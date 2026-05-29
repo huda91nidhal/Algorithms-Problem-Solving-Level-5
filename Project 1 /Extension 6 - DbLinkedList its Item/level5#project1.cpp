c06e6e7392c2ea908cdbdfa9d792ed9dad862f9f
huda91nidhal <hudaalshomar@gmail.com>
2026-05-29 14:49:43 +0300

    Add files via upload

---
+++ Project 1 /Extension 6 - DbLinkedList its Item/level5#project1.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include "clsDbLinkedList.h"
+using namespace std;
+int main()
+{
+	clsDbLinkedList <int> MyDbLinkedList;
+
+	MyDbLinkedList.InsertAtBeginning(5);
+	MyDbLinkedList.InsertAtBeginning(4);
+	MyDbLinkedList.InsertAtBeginning(3);
+	MyDbLinkedList.InsertAtBeginning(2);
+	MyDbLinkedList.InsertAtBeginning(1);
+
+	cout << "Linked List Content :\n";
+	MyDbLinkedList.PrintList();
+
+	cout << "\nitem(2) value is : "<<MyDbLinkedList.GetItem(2) << endl;
+	
+	
+	system("pause > 0");
+
+}
