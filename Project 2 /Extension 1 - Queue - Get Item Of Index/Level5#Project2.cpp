8444b71ce7125a87e7d9f432dbcb38af9621781e
huda91nidhal <hudaalshomar@gmail.com>
2026-05-30 14:21:26 +0300

    Add files via upload

---
+++ Project 2 /Extension 1 - Queue - Get Item Of Index/Level5#Project2.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+#include"clsMyQueue.h"
+#include"clsDbLinkedList.h"
+using namespace std;
+int main()
+{
+    clsMyQueue <int> MyQueue;
+
+    MyQueue.Push(10);
+    MyQueue.Push(20);
+    MyQueue.Push(30);
+    MyQueue.Push(40);
+    MyQueue.Push(50);
+
+    cout << "Queue :\n";
+    MyQueue.Print();
+
+    cout << "\nQueue Size : " << MyQueue.Size() << endl;
+    cout << "Queue Front : " << MyQueue.Front() << endl;
+    cout << "Queue Back : " << MyQueue.Back() << endl;
+
+    cout << "\nQueue after pop() :\n";
+    MyQueue.Pop();
+
+    MyQueue.Print();
+
+    cout << "\n\nItem(2) : " << MyQueue.GetItem(2)<<endl;
+
+    return 0;
+}
