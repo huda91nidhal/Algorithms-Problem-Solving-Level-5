0a4a35e5611cebf000b6849333cd252abd323471
huda91nidhal <hudaalshomar@gmail.com>
2026-05-30 14:29:00 +0300

    Add files via upload

---
+++ Project 2 /Extension 2 - Queue - Reverse/clsMyQueue.h
@@ -0,0 +1,57 @@
+#pragma once
+#include<iostream>
+#include"clsDbLinkedList.h"
+using namespace std;
+template <class T>
+class clsMyQueue
+{
+protected :
+    clsDbLinkedList <T> _MyList;
+public:
+	void Push(T Item)
+	{
+        _MyList.InsertAtEnd(Item);
+    }
+
+    void Pop()
+    {
+        _MyList.DeleteFirstNode();
+    }
+
+    bool IsEmpty()
+    {
+        return _MyList.IsEmpty();
+    }
+
+    void Print()
+    {
+        _MyList.PrintList();
+    }
+
+    int Size()
+    {
+        return _MyList.Size();
+    }
+
+    T Front()
+    {
+        return _MyList.GetItem(0);
+
+    }
+
+    T Back()
+    {
+        return _MyList.GetItem(Size() - 1);
+    } 
+
+    T GetItem(T Index)
+    {
+        return _MyList.GetItem(Index);
+    }
+
+    void Reverse()
+    {
+        return _MyList.Reverse();
+    }
+};
+
