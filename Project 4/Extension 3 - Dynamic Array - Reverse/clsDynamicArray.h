e2f1af8de5bccba2070deb6d8307aec964fa5c4c
huda91nidhal <hudaalshomar@gmail.com>
2026-05-31 17:42:08 +0300

    Add files via upload

---
+++ Project 4/Extension 3 - Dynamic Array - Reverse/clsDynamicArray.h
@@ -0,0 +1,111 @@
+#pragma once
+
+#include <iostream>
+using namespace std;
+
+template <class T>
+class clsDynamicArray
+{
+
+protected:
+    int _Size = 0;
+    T* _TempArry;
+
+public:
+    T* OriginalArray;
+
+    clsDynamicArray(int Size = 0)
+    {
+        if (Size < 0)
+            Size = 0;
+
+        _Size = Size;
+
+        OriginalArray = new T[_Size];
+
+    }
+
+    ~clsDynamicArray()
+    {
+
+        delete[]  OriginalArray;
+
+    }
+
+    bool SetItem(int index, T Value)
+    {
+
+        if (index >= _Size || _Size < 0)
+        {
+            return false;
+        }
+
+        OriginalArray[index] = Value;
+        return true;
+
+    }
+
+
+    int Size()
+    {
+        return _Size;
+    }
+
+    bool IsEmpty()
+    {
+        return (_Size == 0 ? true : false);
+
+    }
+
+    void PrintList()
+
+    {
+
+        for (int i = 0; i <= _Size - 1; i++)
+        {
+            cout << OriginalArray[i] << " ";
+        }
+
+        cout << "\n";
+
+    }
+
+    void Resize(int NewSize)
+    {
+        if (NewSize < 0)
+            NewSize = 0;
+        _TempArry = new T[NewSize];
+
+        if (NewSize < _Size)
+        {
+            _Size = NewSize;
+        }
+
+        for (int i = 0; i < _Size; i++)
+        {
+            _TempArry[i] = OriginalArray[i];
+        }
+        _Size = NewSize;
+        delete[] OriginalArray;
+        OriginalArray = _TempArry;
+    }
+
+    T GetItem(int Index)
+    {
+      return OriginalArray[Index];
+    }
+
+    void Reverse()
+    {
+        _TempArry =  new T[_Size];
+        int Counter = 0;
+        for (int i = _Size - 1 ; i >= 0; i--)
+        {
+           _TempArry[Counter] = OriginalArray[i];
+           Counter++;
+        }
+        delete[] OriginalArray;
+        OriginalArray = _TempArry;
+    }
+
+};
