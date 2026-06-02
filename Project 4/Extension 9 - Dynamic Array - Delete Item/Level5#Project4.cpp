4a75a60762b7af7b33c949d2fb484eed271ac9ab
huda91nidhal <hudaalshomar@gmail.com>
2026-06-02 18:42:20 +0300

    Add files via upload

---
+++ Project 4/Extension 9 - Dynamic Array - Delete Item/Level5#Project4.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+#include"clsDynamicArray.h"
+using namespace std;
+int main()
+{
+    clsDynamicArray <int> MyDynamicArray(5);
+
+    MyDynamicArray.SetItem(0, 10);
+    MyDynamicArray.SetItem(1, 20);
+    MyDynamicArray.SetItem(2, 30);
+    MyDynamicArray.SetItem(3, 40);
+    MyDynamicArray.SetItem(4, 50);
+
+    cout << "Is Empty : " << MyDynamicArray.IsEmpty() << endl;
+    cout << "\nArray Size : " << MyDynamicArray.Size() << endl;
+
+    cout << "\nArray Items : \n";
+    MyDynamicArray.PrintList();
+
+    int Index = MyDynamicArray.Find(50); 
+
+    if (Index == -1)
+        cout << "\nItem was not found :-(\n";
+    else
+        cout << "\n30 is found at Index " << Index << endl;
+    
+    
+    MyDynamicArray.DeleteItem(30);
+    cout << "\nArray Items after delete 30 : \n";
+    cout << "\nArray Size : " << MyDynamicArray.Size() << endl;
+    MyDynamicArray.PrintList();
+
+
+    return 0;
+
+}
+
